b428913033fc72c61f140242aec28472fdb04eb8
Sachintha Heshan <sachintha@leafycode.com>
2021-11-05 19:25:25 +0530

    Feat: Change Console Messagers

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const jsonFile = 'data.json';
 const fs = require('fs');
 
 
-const startIndex = 1347335;
+const startIndex = 1347235;
 const endIndex = 1347339;
 const finalData = []
-let index
+let count = 0;
+let index;
 
 const config = {
     headers: {
@@ -17,10 +18,22 @@ const config = {
 const getData = (index) => {
     axios.get(`https://apphub.mobitel.lk/mobitelint/mapis/selectedstudent/api/student?index=${index}`, config)
     .then((data) => {
+        count++;
+
         if(data.data.id != 0 && data.data.unicode=== "123P"){
             console.log(data.data.indexNo, ' - ' ,data.data.fullName)
             finalData.push(data.data);
         }
+
+        console.clear();
+        console.log(`Found (${finalData.length}) : `);
+
+        finalData.forEach((finalDataOne)=>{
+            console.log('   ',finalDataOne.indexNo, ' - ' ,finalDataOne.fullName)
+        })
+
+        console.log('\n');
+        console.log(`Successfull (${count}) : `,index);
     })
     .finally(()=>{
         if(index==endIndex){
@@ -34,9 +47,5 @@ const getData = (index) => {
 }
 
 getData(startIndex)
-
-// for(let index = startIndex; index<=endIndex; index++){
-//     getData(index);
-// }
 
 
